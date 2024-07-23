a7eff5a70177fb93c334b3bbd939e7639f5775d3
krui <krui0728@gmail.com>
2024-07-23 15:54:25 +0800

    feat: :sparkles: 1. 修改项目的入口

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: krui krui0728@gmail.com
  * @Date: 2024-07-23 11:49:03
  * @LastEditors: krui krui0728@gmail.com
- * @LastEditTime: 2024-07-23 15:27:47
+ * @LastEditTime: 2024-07-23 15:53:53
  * @FilePath: /fe-calc/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
